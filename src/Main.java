18cb477f107f3290a5f4920684be7e97252bc0d7
Ilmira Kudryavtseva <ilshakudz@gmail.com>
2022-02-03 21:14:32 +0400

    Free miles

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int ticketPrice = 4500;
-        int mileFromPrice = 1;
-        int Price = 20;
+        int priceForMile = 20;
         int freeMile;
 
-        freeMile = ticketPrice * mileFromPrice / Price;
+        freeMile = ticketPrice * 1 / priceForMile;
         System.out.println("Вам начислено " + freeMile + " миль");
     }
 }
